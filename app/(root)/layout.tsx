a34ef75e49d3d28bce20db02fd14866f1c32f5df
Francisco Araujo <francisco.araujo@redeflex.com.br>
2024-06-03 23:18:44 -0300

    Adicionando modificaçoes no projeto

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,34 @@
+import { MobileNav } from "@/components/ui/MobileNav";
+import { SideBar } from "@/components/ui/SideBar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName: 'Francisco', lastName: 'Araujo'}
+
   return (
-      <main>
-        SIDEBAR
-        {children}
-      </main>
+    <main className='flex h-screen w-full font-inter'>
+      <SideBar user={loggedIn} />
+
+        <div className='flex size-full flex-col'>
+           <div className='root-layout'>
+             <Image 
+                src='/icons/logo.svg'
+                width={30}
+                height={30}
+                alt='logo'
+             />
+            <div>
+                <MobileNav 
+                  user={loggedIn}/>
+            </div>
+           </div>
+           {children}
+        </div> 
+    </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+
+import HeaderBox from '@/components/ui/HeaderBox'
+import { TotalBalancebox } from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const looogedIn = {firstName:'Franciso Araujo'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+         <header className='home-header'>
+           <HeaderBox 
+              type='greeting'
+              title='Welcome'
+              user={looogedIn?.firstName || 'Guest'}
+              subtext='Access and manage your account and
+              transactions efficiently.'
+           />
+           <TotalBalancebox 
+               accounts={[]}
+               totalBanks={1}
+               totalCurrentBalance={1250.35}
+           />
+         </header>
+      </div>
+    </section>
   )
 }
 
